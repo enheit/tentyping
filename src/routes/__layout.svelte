724034bea71e1dd06733079ac7dc849ffb913453
Roman Mahotskyi <roman.mahotskyi@gmail.com>
2022-05-06 14:01:45 +0300

    Add initial layout to the app

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,31 @@
+<div class="container">
+  <header>header</header>
+
+  <main>
+    <slot />
+  </main>
+
+  <footer>footer</footer>
+</div>
+
+<style>
+  :global(html, body) {
+    height: 100%;
+  }
+
+  :global(body) {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    margin: 0;
+  }
+
+  .container {
+    min-height: 100%;
+    max-width: 1000px;
+    width: 100%;
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    grid-template-columns: 100%;
+  }
+</style>
