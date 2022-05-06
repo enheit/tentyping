ceee88c61e9b61c3a72cbf4934db4b78a3d9d60e
enheit <roman.mahotskyi@gmail.com>
2022-05-06 21:35:39 +0300

    Updates

--- src/global.d.ts
+++
@@ -1,1 +0,0 @@
-declare const PACKAGE_JSON: { version: string };
--- src/routes/index.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import "bootstrap-icons/font/bootstrap-icons.css";
-</script>
-
-<svelte:head>
-  <title>Tentyping</title>
-</svelte:head>
-
-<p>Tentyping body</p>
