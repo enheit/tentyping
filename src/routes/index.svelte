028cff70550d9705d4b78e900df4dafc709ae554
Roman Mahotskyi <roman.mahotskyi@gmail.com>
2022-05-07 22:00:52 +0300

    Add WPM calculation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,6 +8,8 @@
   const sentence = 'lorem ipsum dolor sit amet consectetur adipiscing elit phasellus et turpis lacus donec dolor justo lacinia et odio nec placerat dapibus ex donec accumsan dui quis dolor sollicitudin eu facilisis purus malesuada donec vitae eleifend arcu vel interdum enim'
   const words = sentence.split(/(\s+)/)
 
+  let RAFId: number = -1
+  const wpm = writable(0)
   const typos = writable(0)
   const caretIndex = writable(0)
   const typedSymbols = writable('')
@@ -16,12 +18,42 @@
   $: done = $caretIndex === sentence.length
   $: accuracy = 100 - Math.round($typos / sentence.length * 100)
 
+  $: if (done) {
+    cancelAnimationFrame(RAFId)
+  }
+
+  let startTimestamp: number | null = null
+  let lastTimestamp = 0;
+  let speedInMs = 500;
+
+  function step (timestamp: number): void {
+    if (!startTimestamp) {
+      startTimestamp = timestamp
+    }
+
+    const localTimestamp = timestamp - startTimestamp
+
+    if (timestamp - lastTimestamp >= speedInMs) {
+      lastTimestamp = timestamp;
+
+      const progressInMinutes = (localTimestamp / 1000) / 60
+
+      $wpm = calculateGrossWpm($typedSymbols.length, progressInMinutes || 1) // `... || 1` removes 0 case, which renders Infinity
+    }
+
+    RAFId = requestAnimationFrame(step)
+  }
+
   function reset(): void {
-    console.log(typosIndexes)
+    $wpm = 0
     $caretIndex = 0
     $typos = 0
     $typedSymbols = ''
     $typosIndexes = new Set()
+    startTimestamp = null
+    lastTimestamp = 0
+
+    cancelAnimationFrame(RAFId)
   }
 
   function clearLastSymbol (): void {
@@ -50,6 +82,10 @@
       return
     }
 
+    if ($caretIndex === 0) {
+      RAFId = requestAnimationFrame(step)
+    }
+
     const typedSymbol = event.key
     const expectedSymbol = sentence.at($caretIndex)
     const typo = expectedSymbol !== typedSymbol
@@ -76,6 +112,13 @@
     
     return 'bi-emoji-neutral'
   }
+
+  function calculateGrossWpm (symbols: number, ellapsedTimeInMinutes: number): number {
+    const averageCharacters = 5
+    const grossWpm = Math.round((symbols / averageCharacters) / ellapsedTimeInMinutes)
+
+    return grossWpm
+  }
 </script>
 
 <svelte:head>
@@ -86,6 +129,7 @@
 
 <div class="container">
   <div class="typing-info">
+    <InfoItem icon='bi-lightning' label='words/min' value={$wpm.toString()} />
     <InfoItem icon='bi-percent' label='accuracy' value={`${accuracy}`} />
     <InfoItem icon={getTyposEmoji(0, $typos, done)} label='typos' value={$typos.toString()} />
   </div>
